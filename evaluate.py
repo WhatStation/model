b5b727407622feb68040d61de9d444a55dbd693d
RG-Im <94027045+RK-IM@users.noreply.github.com>
2023-02-12 01:25:05 +0900

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -7,9 +7,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import argparse
 import pandas as pd
 from tabulate import tabulate
+tabulate.WIDE_CHARS_MODE = False
 
-
-def predict_sample():
+def predict_sample():    
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--text",
@@ -22,8 +22,10 @@ def predict_sample():
 
     args = parser.parse_args()
 
-    s_bert = model_builder.load_sbert(model_path=CFG.CLASSIFIER_MODEL_PATH,
-                                    sbert_path=CFG.SBERT_MODEL_FOLDER)
+    tagged_store = pd.read_csv(f"{CFG.DATA_PATH}/{CFG.TAG_CSV}")
+
+    s_bert = model_builder.load_sbert(model_path=CFG.SBERT_MODEL_PATH,
+                                      sbert_path=CFG.SBERT_MODEL_FOLDER)
     classifier = model_builder.get_classifier()
     classifier.load_weights(args.model_path)
 
@@ -33,7 +35,12 @@ def predict_sample():
     res = pd.DataFrame(result, columns=CFG.CLASS_NAMES).T
     res = res.rename(columns={0: 'Probability'})
 
-    print(tabulate(res, headers='keys', tablefmt='psql'))
+    convert_col = dict(zip(tagged_store.iloc[:, -16:-1].columns, CFG.CLASS_NAMES))
+    tagged_store = tagged_store.rename(columns=convert_col)
+
+    store_tag_prob = pd.concat([tagged_store['name'], tagged_store.iloc[:, -16:-1] * res['Probability']], axis=1)
+    store_tag_prob['sum'] = store_tag_prob.iloc[:, -16:-1].sum(axis=1, numeric_only=True)
+
 
     tags = res[res['Probability']>0.5].index
     if len(tags) > 0:
@@ -41,6 +48,16 @@ def predict_sample():
     else:
         print("좀 더 자세한 분위기가 나타나게 작성해주세요!")
 
+
+    if store_tag_prob['sum'].max() < 1:
+        print("좀 더 자세한 분위기가 나타나게 작성해주세요!")
+    else:
+        store_tag_prob = store_tag_prob.sort_values('sum', ascending=False)
+        store_tag_prob = store_tag_prob.T.iloc[:, :5]
+
+        store_tag_prob.columns = store_tag_prob.iloc[0]
+        store_tag_prob = store_tag_prob.drop(store_tag_prob.index[0])
+        print(tabulate(store_tag_prob, headers='keys', tablefmt='psql', floatfmt=".4f"))
 
 if __name__ == "__main__":
     predict_sample()
